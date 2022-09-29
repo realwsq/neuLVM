60baa10b6fc17db48fb44e79db02075d23706aff
realwsq <shuqi.wang@epfl.ch>
2022-09-29 14:48:09 +0200

    update 3pop_BILM.sh so that it works with the small dataset

--- 3pop_BILM.sh
+++ 3pop_BILM.sh
@@ -4,7 +4,7 @@
 for i in {1..5}
 do
 	echo $i
-	python train_meso.py --end_timepoint=10 --train_Nrinit=$i
+	python train_meso.py --end_timepoint=11 --train_Nrinit=$i
 done
 
 # for i in 44 65 85 100 135 140 150 175 195 200
